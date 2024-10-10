1e5f87ca5ad6032a5825cd10efa1ff646c7b6c3a
Bryan <bryanyuen1998@gmail.com>
2024-10-10 17:37:41 -0400

    updated register and login pages

--- client/src/components/loginPage/LoginPage.js
+++ client/src/components/loginPage/LoginPage.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
-import LoginPagePicture from '../../pictures/woman_on_phone_photo.jpg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
+import LoginPagePicture from "../../pictures/woman_on_phone_photo.jpg";
 
 export default function LoginPage() {
   const [loginEmail, setLoginEmail] = useState();
@@ -16,8 +16,8 @@ export default function LoginPage() {
 
   const checkCookie = async () => {
     try {
-      await axios.get('/users/checkCookie');
-      navigate('/Dashboard');
+      await axios.get("/users/checkCookie");
+      navigate("/Dashboard");
     } catch (error) {
       console.log(error);
     }
@@ -26,60 +26,56 @@ export default function LoginPage() {
   const loginHandler = async (e) => {
     e.preventDefault();
     try {
-      console.log('Im in here');
-      await axios.post('/users/login', {
+      console.log("Im in here");
+      await axios.post("/users/login", {
         email: loginEmail,
         password: loginPassword,
       });
-      navigate('/Dashboard');
+      navigate("/Dashboard");
     } catch (error) {
-      alert('Incorrect password');
+      alert("Incorrect password");
     }
   };
 
-  const goToRegister = () => navigate('/Register');
+  const goToRegister = () => navigate("/Register");
 
   return (
-    <div className="login-background">
-      <div class="login-picture-container">
-        <img src={LoginPagePicture}></img>
-      </div>
-      <div className='login-right-side'>
-        <h1 className="login-header">Contact Manager</h1>
-        <div className="login-container">
-          <h2 className="login-box-header">Login</h2>
-          <form onSubmit={loginHandler}>
-            <div class="mb-3">
-              <label class="form-label">Email</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Email"
-                onChange={(e) => setLoginEmail(e.target.value)}
-              ></input>
-            </div>
-            <div class="mb-3">
-              <label class="form-label">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                placeholder="Password"
-                onChange={(e) => setLoginPassword(e.target.value)}
-              ></input>
-            </div>
-            <div class="d-grid gap-2">
-              <button type="submit" class="btn btn-primary">
-                Login
-              </button>
-            </div>
-            <div>
-              <button type="button" class="btn btn-link" onClick={goToRegister}>
-                No account? Click here to sign up
-              </button>
-            </div>
-          </form>
+    <div className="login-page">
+      <form onSubmit={loginHandler} className="login-container">
+        <div className="header-container">
+          <img src="/phone.png" className="header-image"/>
+          <h1 className="login-header">Contact Manager</h1>
         </div>
-      </div>
+        <h2 className="login-box-header">Welcome Back</h2>
+        <div class="input-container">
+          <label class="form-label">Email</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Email"
+            onChange={(e) => setLoginEmail(e.target.value)}
+          ></input>
+        </div>
+        <div class="input-container">
+          <label class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            onChange={(e) => setLoginPassword(e.target.value)}
+          ></input>
+        </div>
+        <div class="input-container">
+          <button type="submit" class="btn btn-primary login-button">
+            Login
+          </button>
+        </div>
+        <div class="sign-up-button-container">
+          <button type="button" class="btn btn-link sign-up-button" onClick={goToRegister}>
+            Don't have an account? Sign up
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/components/registerPage/RegisterPage.js
+++ client/src/components/registerPage/RegisterPage.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import './RegisterPage.css';
-import LoginPagePicture from '../../pictures/woman_on_phone_photo.jpg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./RegisterPage.css";
+import LoginPagePicture from "../../pictures/woman_on_phone_photo.jpg";
 
 export default function RegisterPage() {
   const [registerName, setRegisterName] = useState();
@@ -15,16 +15,17 @@ export default function RegisterPage() {
     checkCookie();
   }, []);
 
-  const register = async () => {
+  const register = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post('/users/register', {
+      const response = await axios.post("/users/register", {
         name: registerName,
         email: registerEmail,
         password: registerPassword,
       });
       console.log(response);
       if (response.status === 201) login();
-      else alert('Email Exists');
+      else alert("Email Exists");
     } catch (error) {
       console.log(error);
     }
@@ -32,13 +33,13 @@ export default function RegisterPage() {
 
   const login = async () => {
     try {
-      const response = await axios.post('/users/login', {
+      const response = await axios.post("/users/login", {
         email: registerEmail,
         password: registerPassword,
       });
       console.log(response);
-      if (response.status === 200) navigate('/Dashboard');
-      else alert('Incorrect password');
+      if (response.status === 200) navigate("/Dashboard");
+      else alert("Incorrect password");
     } catch (error) {
       console.log(error);
     }
@@ -46,73 +47,65 @@ export default function RegisterPage() {
 
   const checkCookie = async () => {
     try {
-      const response = await axios.get('/users/checkCookie');
-      if (response.status === 200) navigate('/Dashboard');
+      const response = await axios.get("/users/checkCookie");
+      if (response.status === 200) navigate("/Dashboard");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const goToLogin = () => navigate('/');
+  const goToLogin = () => navigate("/");
 
   return (
-    <div className="register-background">
-      <div className="register-picture-container">
-        <img src={LoginPagePicture}></img>
-      </div>
-      <div className='register-right-side'>
-        <h1 className="register-header">Contact Manager</h1>
-        <div className="register-container">
-          <h2 className="register-box-header">Register</h2>
-          <form>
-            <div class="mb-3">
-              <label class="form-label">Name</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Name"
-                onChange={(e) => setRegisterName(e.target.value)}
-              ></input>
-            </div>
-            <div class="mb-3">
-              <label class="form-label">Email</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Email"
-                onChange={(e) => setRegisterEmail(e.target.value)}
-              ></input>
-            </div>
-            <div class="mb-3">
-              <label class="form-label">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                placeholder="Password"
-                onChange={(e) => setRegisterPassword(e.target.value)}
-              ></input>
-            </div>
-            <div class="d-grid gap-2">
-              <button
-                type="button"
-                class="btn btn-primary"
-                onClick={() => register()}
-              >
-                Register
-              </button>
-            </div>
-            <div>
-              <button
-                type="button"
-                class="btn btn-link"
-                onClick={() => goToLogin()}
-              >
-                Already have an Account? Click here to Login
-              </button>
-            </div>
-          </form>
+    <div className="register-page">
+      <form onSubmit={register} className="register-container">
+        <div className="header-container">
+          <img src="/phone.png" className="header-image" />
+          <h1 className="register-header">Contact Manager</h1>
         </div>
-      </div>
+        <h2 className="login-box-header">Create your account</h2>
+        <div className="input-container">
+          <label class="form-label">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Name"
+            onChange={(e) => setRegisterName(e.target.value)}
+          ></input>
+        </div>
+        <div class="input-container">
+          <label class="form-label">Email</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Email"
+            onChange={(e) => setRegisterEmail(e.target.value)}
+          ></input>
+        </div>
+        <div class="input-container">
+          <label class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            onChange={(e) => setRegisterPassword(e.target.value)}
+          ></input>
+        </div>
+        <div class="input-container">
+          <button type="submit" class="btn btn-primary register-button">
+            Sign up
+          </button>
+        </div>
+        <div>
+          <button
+            type="button"
+            class="btn btn-link"
+            onClick={() => goToLogin()}
+          >
+            Already have an Account? Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
